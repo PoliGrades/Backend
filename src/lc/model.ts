dc78c6e907ad500656faface9a4373e7bc6ac44b
Mateus S. Pereira <mateusspereira64@gmail.com>
2025-05-07 13:57:33 -0300

    feat: added SocketIO and integrated with frontend

--- src/lc/model.ts
+++ src/lc/model.ts
@@ -1,81 +1,22 @@
-// deno-lint-ignore-file
-import { HumanMessage } from "@langchain/core/messages";
-import { tool } from "@langchain/core/tools";
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 import { ChatMistralAI } from "@langchain/mistralai";
+import { tool } from "@langchain/core/tools";
 import { z } from "zod";
 
 const llm = new ChatMistralAI({
   model: "mistral-small-latest",
-  temperature: 0
+  temperature: 0,
 });
 
-interface RestaurantMenu {
-  name: string;
-  menu: Array<{ item: string; price: number }>;
+const messages = [
+  new SystemMessage(
+    "Você é um assistente de um chatbot chamado 'PoliEats', sua função é prover informações sobre o estabelecimento como: Cardápio, horários de funcionamento, receber pedidos e prover status dos pedidos em andamento. Você responde apenas em português",
+  ),
+];
+
+export async function addMessage(message: string) {
+  messages.push(new HumanMessage(message));
+  return await llm.invoke(messages).then((res) => {
+    return res.content;
+  });
 }
-
-const restaurantMenus: RestaurantMenu[] = [
-    {
-        name: "Pizza Place",
-        menu: [
-        { item: "Margherita Pizza", price: 10 },
-        { item: "Pepperoni Pizza", price: 12 },
-        { item: "Veggie Pizza", price: 11 },
-        ],
-    },
-    {
-        name: "Burger Joint",
-        menu: [
-        { item: "Cheeseburger", price: 8 },
-        { item: "Veggie Burger", price: 7 },
-        { item: "Double Burger", price: 10 },
-        ],
-    },
-]
-
-const getRestaurantMenu = tool(
-  async ({ restaurant_name }) => {
-    const restaurant = restaurantMenus.find(
-      (menu) => menu.name.toLowerCase() === restaurant_name.toLowerCase()
-    );
-    if (!restaurant) {
-      throw new Error(`Restaurant ${restaurant_name} not found.`);
-    }
-    return `Menu for ${restaurant.name}: ${restaurant.menu.map(item => `${item.item} - $${item.price}`).join(", ")}`;
-  }
-  ,
-    {
-        name: "get_restaurant_menu",
-        schema: z.object({
-        restaurant_name: z.string(),
-        }),
-        description: "Get the menu of a restaurant.",
-    }
-);
-
-const tools = [getRestaurantMenu];
-
-const llmWithTools = llm.bindTools(tools);
-
-const messages = [new HumanMessage("What is the menu of Pizza Place?")];
-
-const aiMessage = await llmWithTools.invoke(messages);
-
-console.log(aiMessage);
-
-messages.push(aiMessage);
-
-const toolsByName = {
-    get_restaurant_menu: getRestaurantMenu,
-  };
-  
-  for (const toolCall of aiMessage.tool_calls) {
-    const selectedTool = toolsByName[toolCall.name];
-    const toolMessage = await selectedTool.invoke(toolCall);
-    messages.push(toolMessage);
-  }
-  
-  console.log(messages);
-
-  const final = await llmWithTools.invoke(messages);
-  console.log(final);
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,33 @@
 import express from "express";
+import { Server } from "npm:socket.io";
+import { createServer } from "node:http";
+import cors from "npm:cors";
+import { addMessage } from "./lc/model.ts";
 
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173",
+}));
+
+const wsServer = createServer(app);
+const io = new Server(wsServer, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
 
 app.get("/", (_req, res) => {
   res.send("Welcome to the Dinosaur API!");
 });
 
-app.listen(8000);
+io.on("connection", (socket) => {
+  socket.on("message", async (e) => {
+    await addMessage(e).then((final) => {
+      socket.emit("message", final);
+    });
+  });
+});
+
+wsServer.listen(8000);
 console.log(`Server is running on http://localhost:8000`);
